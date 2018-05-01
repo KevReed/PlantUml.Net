0c32ebc5bee585bb8177c9ac2d2e01b02cbe6fc1
Kevin Reed <kevin.reed@derivco.co.uk>
2018-05-01 17:45:45 +0100

    removed OutputFormat from PlantUmlSettings as is is specified in the call to Render

--- src/PlantUmlSettings.cs
+++ src/PlantUmlSettings.cs
@@ -7,11 +7,9 @@
         public string LocalPlantUmlPath { get; set; }
         public string LocalGraphvizDotPath { get; set; }
         public RenderingMode RenderingMode { get; set; }
-        public OutputFormat OutputFormat { get; set; }
 
         public PlantUmlSettings()
         {
-            OutputFormat = OutputFormat.Png;
             RenderingMode = RenderingMode.Remote;
             RemoteUrl = "http://www.plantuml.com/plantuml/";
             LocalPlantUmlPath = "plantuml.jar";
