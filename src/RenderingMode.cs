524f9d6779ebc5e2b5bd38f82ae2d37df5dcbd44
Kevin Reed <kevin.reed@derivco.co.uk>
2018-05-13 21:41:43 +0100

    added comments for settings

--- src/RenderingMode.cs
+++ src/RenderingMode.cs
@@ -2,7 +2,14 @@
 {
     public enum RenderingMode
     {
+        /// <summary>
+        /// Remote rendering mode uses the PlantUml hosted service to render diagrams.
+        /// </summary>
         Remote,
+
+        /// <summary>
+        /// Local rendering mode uses a local copy of PlantUml to render diagrams.
+        /// </summary>
         Local
     }
 }
