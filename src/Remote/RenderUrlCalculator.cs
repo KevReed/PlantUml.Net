ba828411fff9e5459d548d2fc3c39345c872129a
Kevin Reed <kevin.reed@derivco.co.uk>
2019-02-14 10:30:23 +0000

    exposed ability to get the remote rendering url

---
+++ src/Remote/RenderUrlCalculator.cs
@@ -0,0 +1,18 @@
+﻿namespace PlantUml.Net.Remote
+{
+    internal class RenderUrlCalculator
+    {
+        private readonly UrlFormatMap urlFormatMap;
+
+        public RenderUrlCalculator(UrlFormatMap urlFormatMap)
+        {
+            this.urlFormatMap = urlFormatMap;
+        }
+
+        public string GetRenderUrl(string code, OutputFormat outputFormat)
+        {
+            string urlComponent = PlantUmlTextEncoding.EncodeUrl(code);
+            return urlFormatMap.GetRenderUrl(urlComponent, outputFormat);
+        }
+    }
+}
